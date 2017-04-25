a446d4d6633c37734ddf48230e6bf7fffc2f8576
goth-pl <m.szycko@gmail.com>
2017-04-25 12:17:49 +0200

    Fixed tests description.

--- spec/models/campfire.rb
+++ spec/models/campfire.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'TODO Campfire' do
+describe 'Campfire Model' do
   include RequestHelpers
   include JSONFixtures
 
--- spec/models/campfire_line.rb
+++ spec/models/campfire_line.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'TODO Model' do
+describe 'CampfireLine Model' do
   include RequestHelpers
   include JSONFixtures
 
